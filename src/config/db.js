9ca4824e5e20dbdae67a22f1d6c32c74b44198f8
Sarthak Sharma <74010886+TheGreatSarthak@users.noreply.github.com>
2024-05-27 22:55:40 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const mongoDBUrl =
-  "mongodb+srv://sarthak2232:qhnafgKvCm61bcX2@cluster0.rbe1tcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "";
 
 const connectDB = () => {
   return mongoose.connect(mongoDBUrl);
